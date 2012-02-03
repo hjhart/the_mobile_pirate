89249a328b4a35ed2b8acc5e978095386d63a2d2
James Hart <hjhart@gmail.com>
2012-02-02 23:36:41 -0800

    Downloading action half-baked and the view now has notices/errors.

--- Gemfile
+++ Gemfile
@@ -1,7 +1,6 @@
 source 'http://rubygems.org'
 
 gem 'sinatra'
-gem 'shotgun'
 
 gem 'haml'
 
--- application.rb
+++ application.rb
@@ -1,12 +1,25 @@
 class Application < Sinatra::Base
+  enable :sessions
   set :root, File.dirname(__FILE__)
   set :logging, true
 
   get '/' do
+    @message = session[:message]
     erb :index
   end
   
   post '/download' do
+    movie = params[:movie]
+
+    redirect '/500' if movie.nil?
     
+    output = `cd ~/Sites/the_rotten_pirate && bundle exec rake download['#{movie}']`
+    puts output 
+    if output
+      session[:message] = 'Your download started successfully!'
+    else
+      session[:error] = 'Your download failed for some reason...'
+    end
+    redirect '/'
   end
 end
